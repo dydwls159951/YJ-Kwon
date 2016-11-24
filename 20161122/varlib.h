d5ab647057892bb171f4de5cb501ae55c414ba0d
YJ Kwon <dydwls159951@naver.com>
2016-11-24 11:19:20 +0900

    20161122
    Signed-off-by: YJ Kwon <dydwls159951@naver.com>

---
+++ 20161122/varlib.h
@@ -0,0 +1,8 @@
+int	VLenviron2table(char **);
+int	VLexport(char *);
+char	*VLlookup(char *);
+void	VLlist();
+int	VLstore( char *, char * );
+char	**VLtable2environ();
+int	VLenviron2table(char **);
+
